63081da6486be001552d0de83f91bd0972865d5b
Joakim G <joagre@gmail.com>
2020-11-03 22:38:45 +0100

    first version of docroot

---
+++ priv/docroot/js/mixmesh.js
@@ -0,0 +1,42 @@
+var Mixmesh = (function() {
+    var privateVar = '';
+
+    function privateMethod () {
+        // ...
+    }
+
+    return { // public interface
+        truncateString: = function(fullStr, strLen, separator) {
+            if (fullStr.length <= strLen) return fullStr;
+
+            separator = separator || '...';
+
+            var sepLen = separator.length,
+                charsToShow = strLen - sepLen,
+                frontChars = Math.ceil(charsToShow/2),
+                backChars = Math.floor(charsToShow/2);
+
+            return fullStr.substr(0, frontChars) +
+                separator + fullStr.substr(fullStr.length - backChars);
+        }
+    };
+})();
+
+/*
+$(document).ready(function(){
+    $("button").click(function(){
+        $(this).hide();
+    });
+
+    var overlay =
+        ml("table", {},
+           [ml("tr", {valign: "top"},
+               [ml("th", {}, "bajs"),
+                ml("th", {}, "bajs2")]),
+            ml("tr", {valign: "top"},
+               [ml("td", {}, "foo"),
+                ml("td", {}, "bar")])]);
+
+    $("#aa").append(overlay);
+});
+*/
