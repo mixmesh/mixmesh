dc2e875c34daa0cff7e4a29d34f877f6db686acf
Joakim G <jocke@gmail.com>
2020-11-04 19:19:22 +0100

    full speed ahead

--- priv/docroot/js/list_keys.js
+++ priv/docroot/js/list_keys.js
@@ -6,6 +6,22 @@ var ListKeys = (function() {
     }
     
     return {
+        deleteKey: function(deleteKeyButton, nym) {
+            Mixmesh.post(
+                "/dj/key/delete", [nym],
+                function(data, textStatus, _jqXHR) {
+                    console.log("/dj/key/delete (POST) succeeded");
+                    console.log(nym + " has been deleted");
+                    console.log("data: " + data);
+                    console.log("textStatus: " + textStatus);
+                    $(deleteKeyButton).closest("tr").remove();
+                },
+                function(_jqXHR, textStatus, errorThrown) {
+                    console.log("/dj/key/delete (POST) failed");
+                    console.log("textStatus: " + textStatus);
+                    console.log("errorThrown: " + errorThrown);
+                });
+        },
         truncate: function(fullStr, strLen, separator) {
             if (fullStr.length <= strLen) {
                 return fullStr;
@@ -24,27 +40,37 @@ var ListKeys = (function() {
 $(document).ready(function() {
     Mixmesh.setHeight("#key-table",
                       ["#navigation", "#search", "#key-table-buttons"]);
-    $.get("/dj/key",
-          function(keys, status) {
-              if (status == "success") {
-                  $.each(keys, function(_index, key) {
-                      var row =
-                          ml("tr", {},
-                             [ml("td", {},
-                                 ml("input", {class: "uk-checkbox",
-                                              type: "checkbox"})),
-                              ml("td", {}, key.nym),
-                              ml("td", {class: "uk-table-link"},
-                                 ml("a", {class: "uk-link-reset", href: "#0"},
-                                    ListKeys.truncate(
-                                        key["public-key"], 32, "..."))),,
-                              ml("td", {},
-                                 ml("span", {"uk-icon": "trash"}))]);
-                      //console.log(key);
-                      $("#key-table-body").append(row);
-                  });
-              } else {
-                  console.log("/dj/key (GET) failed");
-              }
-          });
+    Mixmesh.get(
+        "/dj/key",
+        function(data, status) {
+            if (status == "success") {
+                $.each(data, function(_index, key) {
+                    var row =
+                        ml("tr", {},
+                           [ml("td", {},
+                               ml("input", {class: "uk-checkbox",
+                                            type: "checkbox"})),
+                            ml("td", {}, key.nym),
+                            ml("td", {class: "uk-table-link"},
+                               ml("a", {class: "uk-link-reset", href: "#0"},
+                                  ListKeys.truncate(
+                                      key["public-key"], 32, "..."))),,
+                            ml("td", {},
+                               ml("a", {
+                                   href: "#0", class: "uk-icon-button",
+                                   "uk-icon": "trash",
+                                   onclick: function() {
+                                       ListKeys.deleteKey(this, key.nym);
+                                   }}))]);
+
+                    jml.render($("#key-table-body"), row);
+
+
+                    //$("#key-table-body").append(row);
+                });
+            } else {
+                console.log("/dj/key (GET) failed");
+                console.log(data);
+            }
+        });
 });
--- priv/docroot/js/mixmesh.js
+++ priv/docroot/js/mixmesh.js
@@ -12,6 +12,18 @@ var Mixmesh = (function() {
                 targetHeight -= $(siblingIds[i]).outerHeight(true);
             }
             $(targetId).outerHeight(targetHeight.toString() + "px", true);
+        },
+        get: function(url, success) {
+            $.get(url, null, success);
+        },
+        post: function(url, data, success, error) {
+            $.ajax(url, {
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                type: "POST",
+                success: success,
+                error: error
+            });
         }
     };
 })();
