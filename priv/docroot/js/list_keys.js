9272ba5a37838b718261b714ff1eaed26cc3c6a4
Joakim G <jocke@gmail.com>
2020-11-04 15:40:26 +0100

    full speed ahead

--- priv/docroot/js/list_keys.js
+++ priv/docroot/js/list_keys.js
@@ -4,18 +4,17 @@ var ListKeys = (function() {
     function privateMethod () {
         // ...
     }
-
+    
     return {
         truncate: function(fullStr, strLen, separator) {
-            if (fullStr.length <= strLen) return fullStr;
-
+            if (fullStr.length <= strLen) {
+                return fullStr;
+            }
             separator = separator || '...';
-
-            var sepLen = separator.length,
-                charsToShow = strLen - sepLen,
-                frontChars = Math.ceil(charsToShow/2),
-                backChars = Math.floor(charsToShow/2);
-
+            var sepLen = separator.length;
+            var charsToShow = strLen - sepLen;
+            var frontChars = Math.ceil(charsToShow/2);
+            var backChars = Math.floor(charsToShow/2);
             return fullStr.substr(0, frontChars) +
                 separator + fullStr.substr(fullStr.length - backChars);
         }
@@ -23,6 +22,8 @@ var ListKeys = (function() {
 })();
 
 $(document).ready(function() {
+    Mixmesh.setHeight("#key-table",
+                      ["#navigation", "#search", "#key-table-buttons"]);
     $.get("/dj/key",
           function(keys, status) {
               if (status == "success") {
@@ -39,8 +40,8 @@ $(document).ready(function() {
                                         key["public-key"], 32, "..."))),,
                               ml("td", {},
                                  ml("span", {"uk-icon": "trash"}))]);
-                      console.log(key);
-                      $("#tbody-keys").append(row);
+                      //console.log(key);
+                      $("#key-table-body").append(row);
                   });
               } else {
                   console.log("/dj/key (GET) failed");
