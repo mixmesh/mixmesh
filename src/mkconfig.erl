576a6e3948f7305d17552839579a00283b0bab0c
Joakim G <jocke@gmail.com>
2021-03-23 18:01:43 +0100

    Added a webkey-service configurable to config files

--- src/mkconfig.erl
+++ src/mkconfig.erl
@@ -1,5 +1,5 @@
 -module(mkconfig).
--export([start/1, start/3, create_player/4, ensure_libs/3]).
+-export([start/1, start/3, copy_certificate/3, create_player/4, ensure_libs/3]).
 
 %% This script creates the appropriate file structure needed to start
 %% Mixmesh. You call this command with an mixmesh directory and the
@@ -7,7 +7,7 @@
 %% If you do this the following will be created:
 %%
 %% /tmp/mixmesh/pin
-%% /tmp/mixmesh/global-pki
+%% /tmp/mixmesh/global-pki/ssl
 %% /tmp/mixmesh/alice/player/temp/
 %% /tmp/mixmesh/alice/player/buffer/
 %% /tmp/mixmesh/alice/player/local-pki/
@@ -24,10 +24,13 @@
 
 -spec start([string()]) -> no_return().
 
+%% Called from mixmesh/bin/mkconfig
 start([MixmeshDir, SourceCertFilename, Nym]) ->
     GlobalPkiDir = filename:join([MixmeshDir, <<"global-pki">>]),
+    SSLDir = filename:join([GlobalPkiDir, <<"ssl">>]),
     try
-        true = ensure_libs(stdout, [GlobalPkiDir], true),
+        true = ensure_libs(stdout, [SSLDir], true),
+        true = copy_certificate(stdout, SourceCertFilename, SSLDir),
         true = create_player(stdout, MixmeshDir, SourceCertFilename, Nym),
         PinFilename = filename:join([MixmeshDir, <<"pin">>]),
         io:format("Creates dummy ~s\n", [PinFilename]),
@@ -40,15 +43,33 @@ start([MixmeshDir, SourceCertFilename, Nym]) ->
 
 -spec start(binary(), binary(), binary()) -> boolean().
 
+%% Called from player/src/rest_bootstrap_server.erl
 start(MixmeshDir, SourceCertFilename, Nym) ->
     GlobalPkiDir = filename:join([MixmeshDir, <<"global-pki">>]),
+    SSLDir = filename:join([GlobalPkiDir, <<"ssl">>]),
     try
-        true = ensure_libs(log, [GlobalPkiDir], true),
+        true = ensure_libs(log, [SSLDir], true),
+        true = copy_certificate(log, SourceCertFilename, SSLDir),
         create_player(log, ?b2l(MixmeshDir), ?b2l(SourceCertFilename),
                       ?b2l(Nym))
     catch
         throw:{status, _Status} ->
             false
+    end.
+
+%% Exported: copy_certificate
+
+copy_certificate(TraceMode, SourceCertFilename, SSLDir) ->
+    TargetCertFilename = filename:join([SSLDir, <<"cert.pem">>]),
+    format(TraceMode, "Copies ~s to ~s\n",
+           [SourceCertFilename, TargetCertFilename]),
+    case file:copy(SourceCertFilename, TargetCertFilename) of
+        {ok, _} ->
+            true;
+        {error, Reason} ->
+            format(TraceMode, standard_error, "~s: ~s\n",
+                   [SourceCertFilename, file:format_error(Reason)]),
+            throw({status, 100})
     end.
 
 %% Exported: create_player
@@ -62,22 +83,12 @@ create_player(TraceMode, MixmeshDir, SourceCertFilename, Nym) ->
     LocalPkiDir = filename:join([PlayerDir, <<"local-pki">>]),
     SpoolerDir = filename:join([PlayerDir, <<"spooler">>]),
     ReceivedMessagesDir = filename:join([PlayerDir, <<"received-messages">>]),
-    SSLDir = filename:join([PlayerDir, "ssl"]),
+    SSLDir = filename:join([PlayerDir, <<"ssl">>]),
     true = ensure_libs(TraceMode,
                        [TempDir, BufferDir, LocalPkiDir, SpoolerDir,
                         ReceivedMessagesDir, SSLDir],
                        true),
-    TargetCertFilename = filename:join([SSLDir, <<"cert.pem">>]),
-    format(TraceMode, "Copies ~s to ~s\n",
-           [SourceCertFilename, TargetCertFilename]),
-    case file:copy(SourceCertFilename, TargetCertFilename) of
-        {ok, _} ->
-            true;
-        {error, Reason} ->
-            format(TraceMode, standard_error, "~s: ~s\n",
-                   [SourceCertFilename, file:format_error(Reason)]),
-            throw({status, 100})
-    end.
+    copy_certificate(TraceMode, SourceCertFilename, SSLDir).
 
 %% Exported: ensure_libs
 
