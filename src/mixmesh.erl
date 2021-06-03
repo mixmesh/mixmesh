f5fe1e7416897f321b240092ec0bdf811f9abab0
Joakim G <jocke@gmail.com>
2021-06-03 19:53:06 +0200

    unifying nomenaclature

--- src/mixmesh.erl
+++ src/mixmesh.erl
@@ -1,12 +1,12 @@
 -module(mixmesh).
 -export([start/0, start_bootstrap/0]).
--export([export_public_key/0]).
--export([import_public_key/1, import_public_key/2]).
--export([list_public_keys/0]).
+%%-export([export_public_key/0]).
+%%-export([import_public_key/1, import_public_key/2]).
+%%-export([list_public_keys/0]).
 %%-export([emit_key_pair/0]).
 -export([status/0]).
 
--include_lib("keydir/include/keydir_serv.hrl").
+%%-include_lib("keydir/include/keydir_serv.hrl").
 
 %% Exported: start
 
@@ -82,53 +82,53 @@ start_bootstrap() ->
 
 %% Exported: list_public_keys
 
-list_public_keys() ->
-    ets:foldl(
-      fun(#keydir_user{nym=Nym,public_key=Pk}, _Acc) ->
-	      MD5 = crypto:hash(md5, elgamal:public_key_to_binary(Pk)),
-	      Fs = [tl(integer_to_list(B+16#100,16)) || <<B>> <= MD5],
-	      io:format("~16s -- ~s\n", [Nym, string:join(Fs, ":")])
-      end, ok, keydir_db).
+%% list_public_keys() ->
+%%     ets:foldl(
+%%       fun(#keydir_user{nym=Nym,public_key=Pk}, _Acc) ->
+%% 	      MD5 = crypto:hash(md5, elgamal:pk_to_binary(Pk)),
+%% 	      Fs = [tl(integer_to_list(B+16#100,16)) || <<B>> <= MD5],
+%% 	      io:format("~16s -- ~s\n", [Nym, string:join(Fs, ":")])
+%%       end, ok, keydir_db).
 
-%% Exported: export_public_key
+%% %% Exported: export_public_key
 
-%% export a public key in a format useful for keydir
-export_public_key() ->
-    case config:lookup([player, enabled]) of
-	true ->
-	    Nym = config:lookup([player, username]),
-	    case config:lookup([player, routing, 'public-key'], false) of
-		false ->
-		    false;
-		Pk ->
-		    KeydirUser =
-                        #keydir_user{
-                           nym=Nym,
-                           password=(<<"">>),
-                           email=(<<"">>),
-                           public_key=elgamal:binary_to_public_key(Pk)},
-		    <<"Keydir:",
-		      (base64:encode(term_to_binary(KeydirUser)))/binary>>
-	    end;
-	false ->
-	    false
-    end.
+%% %% export a public key in a format useful for keydir
+%% export_public_key() ->
+%%     case config:lookup([player, enabled]) of
+%% 	true ->
+%% 	    Nym = config:lookup([player, username]),
+%% 	    case config:lookup([player, routing, 'public-key'], false) of
+%% 		false ->
+%% 		    false;
+%% 		Pk ->
+%% 		    KeydirUser =
+%%                         #keydir_user{
+%%                            nym=Nym,
+%%                            password=(<<"">>),
+%%                            email=(<<"">>),
+%%                            public_key=elgamal:binary_to_pk(Pk)},
+%% 		    <<"Keydir:",
+%% 		      (base64:encode(term_to_binary(KeydirUser)))/binary>>
+%% 	    end;
+%% 	false ->
+%% 	    false
+%%     end.
 
-%% Exported: import_public_key
+%% %% Exported: import_public_key
 
-import_public_key(Key) ->
-    import_public_key(Key, undefined).
+%% import_public_key(Key) ->
+%%     import_public_key(Key, undefined).
 
-import_public_key(<<"Keydir:",KeydirUser64/binary>>, Nym) when
-      is_binary(Nym); Nym =:= undefined ->
-    KeydirUserBin = base64:decode(KeydirUser64),
-    KeydirUser0 = binary_to_term(KeydirUserBin),
-    KeydirUser = if Nym =:= undefined -> KeydirUser0;
-		 true -> KeydirUser0#keydir_user{nym=Nym}
-                 end,
-    case keydir_serv:create(KeydirUser) of
-	{error, user_already_exists} ->
-	    keydir_serv:update(KeydirUser);
-	Res ->
-	    Res
-    end.
+%% import_public_key(<<"Keydir:",KeydirUser64/binary>>, Nym) when
+%%       is_binary(Nym); Nym =:= undefined ->
+%%     KeydirUserBin = base64:decode(KeydirUser64),
+%%     KeydirUser0 = binary_to_term(KeydirUserBin),
+%%     KeydirUser = if Nym =:= undefined -> KeydirUser0;
+%% 		 true -> KeydirUser0#keydir_user{nym=Nym}
+%%                  end,
+%%     case keydir_serv:create(KeydirUser) of
+%% 	{error, user_already_exists} ->
+%% 	    keydir_serv:update(KeydirUser);
+%% 	Res ->
+%% 	    Res
+%%     end.
