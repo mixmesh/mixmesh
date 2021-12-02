26f15684324fcde8f1a841186e61cdb800339a89
Joakim G <jocke@gmail.com>
2021-12-02 18:39:58 +0100

    preparing a base version of the gaia app

--- src/mixmesh.erl
+++ src/mixmesh.erl
@@ -28,6 +28,12 @@ start() ->
     ok = application:start(elgamal),
     ok = application:start(enacl),
     ok = application:start(pgp),
+%    case config:lookup([gaia, enabled]) of
+%        true ->
+%            ok = application:start(gaia);
+%        false ->
+%            skip
+%    end,
     case config:lookup([player, enabled]) of
         true ->
             ok = application:start(player);
@@ -76,60 +82,3 @@ start_bootstrap() ->
     ok = application:set_env(
            mixmesh, mode, bootstrap, [{persistent, true}]),
     ok = application:start(mixmesh).
-
-%% Utilities
-
-%% FIXME: Remove the utilities below and put them out of the way
-
-%% Exported: list_public_keys
-
-%% list_public_keys() ->
-%%     ets:foldl(
-%%       fun(#keydir_user{nym=Nym,public_key=Pk}, _Acc) ->
-%% 	      MD5 = crypto:hash(md5, elgamal:pk_to_binary(Pk)),
-%% 	      Fs = [tl(integer_to_list(B+16#100,16)) || <<B>> <= MD5],
-%% 	      io:format("~16s -- ~s\n", [Nym, string:join(Fs, ":")])
-%%       end, ok, keydir_db).
-
-%% %% Exported: export_public_key
-
-%% %% export a public key in a format useful for keydir
-%% export_public_key() ->
-%%     case config:lookup([player, enabled]) of
-%% 	true ->
-%% 	    Nym = config:lookup([player, username]),
-%% 	    case config:lookup([player, routing, 'public-key'], false) of
-%% 		false ->
-%% 		    false;
-%% 		Pk ->
-%% 		    KeydirUser =
-%%                         #keydir_user{
-%%                            nym=Nym,
-%%                            password=(<<"">>),
-%%                            email=(<<"">>),
-%%                            public_key=elgamal:binary_to_pk(Pk)},
-%% 		    <<"Keydir:",
-%% 		      (base64:encode(term_to_binary(KeydirUser)))/binary>>
-%% 	    end;
-%% 	false ->
-%% 	    false
-%%     end.
-
-%% %% Exported: import_public_key
-
-%% import_public_key(Key) ->
-%%     import_public_key(Key, undefined).
-
-%% import_public_key(<<"Keydir:",KeydirUser64/binary>>, Nym) when
-%%       is_binary(Nym); Nym =:= undefined ->
-%%     KeydirUserBin = base64:decode(KeydirUser64),
-%%     KeydirUser0 = binary_to_term(KeydirUserBin),
-%%     KeydirUser = if Nym =:= undefined -> KeydirUser0;
-%% 		 true -> KeydirUser0#keydir_user{nym=Nym}
-%%                  end,
-%%     case keydir_serv:create(KeydirUser) of
-%% 	{error, user_already_exists} ->
-%% 	    keydir_serv:update(KeydirUser);
-%% 	Res ->
-%% 	    Res
-%%     end.
