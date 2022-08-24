f685899b1a9b36cbfb2af08ed0a6ac5d6967d168
Joakim G <jocke@gmail.com>
2022-08-24 11:29:19 +0200

    preparing for boot on pi

--- bin/piboot.sh
+++ bin/piboot.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-if [ ! -z "${MIXMESH_BASE}" ]; then
-   export MIXMESH_BASE=/home/pi/erlang
+if [ "${USER}" != "root" ]; then
+    >&2 echo "Must be root user"
+    exit 1
+fi
+
+if [ -z "${MIXMESH_BASE}" ]; then
+    export MIXMESH_BASE=/home/pi/erlang
+fi
+
+if [ -z "${BT_DEVICE}" ]; then
+    >&2 echo "BT_DEVICE is not set"
+    exit 1
+fi
+
+if [ -z "${PLAYER}" ]; then
+    >&2 echo "PLAYER is not set"
+    exit 1
 fi
 
 if [ ! -z "${BT_DEVICE}" ]; then
-   export BT_DEVICE=20_74_CF_C4_F4_A0
-fi
-
-if [ ! -z "${PLAYER}" ]; then
-   export PLAYER=mother
+    /usr/bin/hcitool cmd 0x3F 0x01C 0x01 0x02 0x00 0x01 0x01
+    sudo -E -u pi /usr/bin/pacmd set-card-profile bluez_card.${BT_DEVICE} headset_head_unit
+    sudo -E -u pi /usr/bin/pacmd set-default-sink bluez_card.${BT_DEVICE}.headset_head_unit
+    sudo -E -u pi /usr/bin/pacmd set-default-source bluez_card.${BT_DEVICE}.headset_head_unit
 fi
 
 sudo -E -u pi ${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached
