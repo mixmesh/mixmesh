245b78c918aa2c18077a39830c52201d6bef7b91
Joakim G <jocke@gmail.com>
2022-08-24 12:04:58 +0200

    preparing for boot on pi

--- bin/piboot.sh
+++ bin/piboot.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "${USER}" != "root" ]; then
-    >&2 echo "Must be root user"
-    exit 1
-fi
-
 if [ -z "${MIXMESH_BASE}" ]; then
     MIXMESH_BASE=/home/pi/erlang
 fi
@@ -14,4 +9,11 @@ if [ -z "${PLAYER}" ]; then
     exit 1
 fi
 
-su --login pi -c "${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached"
+if [ -n "${BT_HEADSET}" ]; then
+    sudo hcitool cmd 0x3F 0x01C 0x01 0x02 0x00 0x01 0x01
+    pacmd set-card-profile bluez_card.${BT_HEADSET} headset_head_unit
+    pacmd set-default-sink bluez_card.${BT_HEADSET}.headset_head_unit
+    pacmd set-default-source bluez_card.${BT_HEADSET}.headset_head_unit
+fi
+
+${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached
