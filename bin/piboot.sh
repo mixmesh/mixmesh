4d3342f2a62fbab5ccc8cde88eea9d5ee0dab320
Joakim G <jocke@gmail.com>
2022-08-24 15:01:19 +0200

    preparing for boot on pi

--- bin/piboot.sh
+++ bin/piboot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pulseaudio --start
+
 if [ -z "${MIXMESH_BASE}" ]; then
     MIXMESH_BASE=/home/pi/erlang
 fi
@@ -12,8 +14,8 @@ fi
 if [ -n "${BT_HEADSET}" ]; then
     sudo hcitool cmd 0x3F 0x01C 0x01 0x02 0x00 0x01 0x01
     pacmd set-card-profile bluez_card.${BT_HEADSET} headset_head_unit
-    pacmd set-default-sink bluez_card.${BT_HEADSET}.headset_head_unit
-    pacmd set-default-source bluez_card.${BT_HEADSET}.headset_head_unit
+    pacmd set-default-sink bluez_sink.${BT_HEADSET}.headset_head_unit
+    pacmd set-default-source bluez_source.${BT_HEADSET}.headset_head_unit
 fi
 
 ${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached
