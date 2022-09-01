ffbd10243c60968e63650873d65c1a291eb13357
Joakim G <jocke@gmail.com>
2022-09-01 16:24:08 +0200

    fixed startup problems

--- bin/piboot.sh
+++ bin/piboot.sh
@@ -12,10 +12,11 @@ if [ -z "${PLAYER}" ]; then
 fi
 
 if [ -n "${BT_HEADSET}" ]; then
-    sudo hcitool cmd 0x3F 0x01C 0x01 0x02 0x00 0x01 0x01
-    pacmd set-card-profile bluez_card.${BT_HEADSET} headset_head_unit
-    pacmd set-default-sink bluez_sink.${BT_HEADSET}.headset_head_unit
-    pacmd set-default-source bluez_source.${BT_HEADSET}.headset_head_unit
+    touch /tmp/piboot.log
+    sudo hcitool cmd 0x3F 0x01C 0x01 0x02 0x00 0x01 0x01 > /tmp/piboot.log 2>&1
+    pacmd set-card-profile bluez_card.${BT_HEADSET} headset_head_unit > /tmp/piboot.log 2>&1
+    pacmd set-default-sink bluez_sink.${BT_HEADSET}.headset_head_unit > /tmp/piboot.log 2>&1
+    pacmd set-default-source bluez_source.${BT_HEADSET}.headset_head_unit > /tmp/piboot.log 2>&1
 fi
 
-${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached
+${MIXMESH_BASE}/mixmesh/bin/mixmesh --config ${MIXMESH_BASE}/mixmesh/etc/${PLAYER}.conf -- -detached  > /tmp/piboot.log 2>&1
